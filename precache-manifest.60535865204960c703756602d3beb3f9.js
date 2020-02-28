1e8b3ac1a5e06490584be723ace8db708739ed98
Yun <melongbob@gmail.com>
2020-02-28 10:50:38 -0800

    Updates

---
+++ precache-manifest.60535865204960c703756602d3beb3f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58e70876fc0f58b62a732e7735423c1",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "eef2f4e9cb1adb32021f",
+    "url": "/pokedex/static/css/main.54090ba6.chunk.css"
+  },
+  {
+    "revision": "9bdd31074b87a980b6fe",
+    "url": "/pokedex/static/js/2.1b3deaa7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokedex/static/js/2.1b3deaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef2f4e9cb1adb32021f",
+    "url": "/pokedex/static/js/main.d3007557.chunk.js"
+  },
+  {
+    "revision": "c2d3ab17bfc57c41f76f",
+    "url": "/pokedex/static/js/runtime-main.0a1f3015.js"
+  }
+]);
